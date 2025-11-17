ed194492ec1cf2f9090bd23e0ef6e3cb6f8813f3
Pavlethegreat123 <pavleadzovic100@gmail.com>
2025-11-17 12:34:32 -0500

    adding user entitiy stuff

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -3,10 +3,18 @@ package entity;
 public class User implements Entity {
     private final String name;
     private final String password;
-    public User(String name, String password) {
+    private String location;
+    private String email;
+    private String phone;
+
+    public User(String name, String password, String location, String email, String phone) {
         this.name = name;
         this.password = password;
+        this.location = location;
+        this.email = email;
+        this.phone = phone;
     }
+
     public String getIdentifier() {
         return name;
     }
