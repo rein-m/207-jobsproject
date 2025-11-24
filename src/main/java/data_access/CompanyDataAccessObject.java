bff074d12217ffe28fca850ac01eeaedc7cfa2c3
Poggers69-PNG <72159679+Poggers69-PNG@users.noreply.github.com>
2025-11-24 13:40:20 -0500

    Implemented the UserDataAccessObject and made minor tweaks to LoginUserDataAccessInterface and SignupUserDataAccessInterface

---
+++ src/main/java/data_access/CompanyDataAccessObject.java
@@ -0,0 +1,21 @@
+package data_access;
+
+// import statements
+
+import entity.Company;
+import entity.CompanyFactory;
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.logout.LogoutUserDataAccessInterface;
+import use_case.post_job.PostJobUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/*
+ DAO for a company.
+*/
+
+public class CompanyDataAccessObject {
+}
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -2,13 +2,15 @@ package entity;
 
 public class User implements Entity {
     private final String name;
+    private final String username;
     private final String password;
     private String location;
     private String email;
     private String phone;
 
-    public User(String name, String password, String location, String email, String phone) {
+    public User(String name, String username, String password, String location, String email, String phone) {
         this.name = name;
+        this.username = username;
         this.password = password;
         this.location = location;
         this.email = email;
@@ -18,7 +20,9 @@ public class User implements Entity {
     public String getIdentifier() {
         return name;
     }
-    public String getPassword() {
-        return password;
-    }
+    public String getUsername() {return username;}
+    public String getPassword() {return password;}
+    public String getLocation() {return location;}
+    public String getEmail() {return email;}
+    public String getPhone() {return phone;}
 }
