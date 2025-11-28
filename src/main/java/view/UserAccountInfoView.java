06eeb461db1aea1108e0d85a6ca10da3e5f9774e
Pavlethegreat123 <pavleadzovic100@gmail.com>
2025-11-28 17:40:50 -0500

    made the view.actionlistener(), now implementing controller, (ONLY MADE / IMPORTED NECESSARY FILES - NOT 1:1 WITH EXAMPLE)

--- src/main/java/view/UserAccountInfoView.java
+++ src/main/java/view/UserAccountInfoView.java
@@ -1,17 +1,19 @@
-package view;
+package view;//package view;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.util.Map;
-
-import interface_adapter.accountmanagement.AccountManagmentController;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import interface_adapter.addResume.AddResumeController;
+//import interface_adapter.addResume.LoginState; // This acc has to be the account mamangement state
+//import interface_adapter.addResume.LoginViewModel;
 
 public class UserAccountInfoView extends JFrame {
+    private AddResumeController addResumeController = null;
 
     public UserAccountInfoView() {
         setTitle("User Information");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 500);
+        setSize(600, 700);
 
         // Main container
         JPanel mainPanel = new JPanel(new BorderLayout(20, 20));
@@ -35,29 +37,21 @@ public class UserAccountInfoView extends JFrame {
         JLabel iconLabel = new JLabel("Basic Information:");
         iconLabel.setFont(new Font("SansSerif", Font.BOLD, 28));
         leftSide.add(iconLabel);
-        JButton btn = new JButton("Edit Account Info");
+        JButton btn = new JButton(">");
         btn.setFocusable(false);
-        btn.setPreferredSize(new Dimension(100, 5));
-        btn.addActionListener(new ActionListener() {
-             @Override
-             public void actionPerformed(ActionEvent e) {
-                 new AccountManagmentController("editUI");
-             }
-        });
+        btn.setPreferredSize(new Dimension(30, 5));
         contactLabel.add(leftSide, BorderLayout.WEST);
         contactLabel.add(btn, BorderLayout.EAST);
         leftPanel.add(contactLabel);
-
-//        JPanel contactLabel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
-//        JLabel text = new JLabel("Contact Information:");
-//        text.setFont(new Font("SansSerif", Font.BOLD, 20));
-//        contactLabel.add(text);
-//        leftPanel.add(contactLabel);
+        leftPanel.add(Box.createVerticalStrut(25));
 
         // Row helper
         leftPanel.add(createInfoRow("✉", "User Email"));
+        leftPanel.add(Box.createVerticalStrut(5));
         leftPanel.add(createInfoRow("☎", "User Phone Number"));
+        leftPanel.add(Box.createVerticalStrut(5));
         leftPanel.add(createInfoRow("📍", "User Address"));
+        leftPanel.add(Box.createVerticalStrut(5));
 
         // Resume Title
         JPanel resume = new JPanel(new BorderLayout());
@@ -68,30 +62,48 @@ public class UserAccountInfoView extends JFrame {
         JButton res_btn = new JButton("Add New Resume");
         res_btn.setFocusable(false);
         res_btn.setPreferredSize(new Dimension(125, 5));
-        btn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new AccountManagmentController("resume");
-            }
-        });
         resume.add(res_leftSide, BorderLayout.WEST);
         resume.add(res_btn, BorderLayout.EAST);
         leftPanel.add(resume);
         leftPanel.add(Box.createVerticalStrut(25));
 
+        // Adding the actionperformed for add new resume
+        res_btn.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {addResumeController.execute();}
+                }
+        );
+
         // Resume File
-        JPanel curr_resume = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
-        // curr_resume.setLayout(new BoxLayout(curr_resume, BoxLayout.Y_AXIS));
-        curr_resume.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        // Main resume panel
+        JPanel main = new JPanel(new BorderLayout());
+        main.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-        JLabel resume_name = new JLabel("File Name");
-        resume_name.setFont(new Font("SansSerif", Font.ITALIC, 16));
+        JPanel all_resumes = new JPanel();
+        all_resumes.setLayout(new BoxLayout(all_resumes, BoxLayout.Y_AXIS));
 
-        JLabel date_added = new JLabel("Date Added");
-        date_added.setFont(new Font("SansSerif", Font.ITALIC, 16));
+        for (int i = 0; i < 20; i++) {
+            JPanel single_resume = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+            single_resume.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+            JLabel resume_name = new JLabel("File Name" + i);
+            resume_name.setFont(new Font("SansSerif", Font.ITALIC, 16));
 
-        curr_resume.add(resume_name); curr_resume.add(date_added);
-        leftPanel.add(curr_resume);
+            JLabel date_added = new JLabel("Date Added");
+            date_added.setFont(new Font("SansSerif", Font.ITALIC, 16));
+
+            single_resume.add(resume_name);
+            single_resume.add(date_added);
+            single_resume.setPreferredSize(new Dimension(300, 50));
+
+            all_resumes.add(single_resume, BorderLayout.SOUTH);
+            all_resumes.add(Box.createVerticalStrut(10));
+        }
+        JScrollPane scrollPane = new JScrollPane(all_resumes);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        main.add(scrollPane, BorderLayout.CENTER);
+        leftPanel.add(main);
         leftPanel.add(Box.createVerticalStrut(25));
 
         // Qualifications Title
@@ -106,7 +118,6 @@ public class UserAccountInfoView extends JFrame {
         qualifications.add(qua_leftSide, BorderLayout.WEST);
         qualifications.add(qua_btn, BorderLayout.EAST);
         leftPanel.add(qualifications);
-
 
         setLocationRelativeTo(null);
     }
