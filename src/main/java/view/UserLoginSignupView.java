e6040a4cfd985d7e36b3130ef06a0a486633fe94
Rein McKelway <rein.mckelway@mail.utoronto.ca>
2025-11-30 14:45:31 -0500

    landing + first nagivation layer

--- src/main/java/view/UserLoginSignupView.java
+++ src/main/java/view/UserLoginSignupView.java
@@ -62,7 +62,7 @@ public class UserLoginSignupView extends JPanel implements ActionListener, Prope
                 new ActionListener() {
                     public void actionPerformed(ActionEvent evt) {
                         if (evt.getSource().equals(logIn)) {
-                            LoginState currentState = loginViewModel.getState();
+                            // switch to Userloggedin LoginState currentState = loginViewModel.getState();
 
                             loginController.execute("User",
                                     currentState.getIdentifier(),
