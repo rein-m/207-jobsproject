5b713ba00c7eb1ed5bd7706bfa77e6b4fe7f43cf
Poggers69-PNG <72159679+Poggers69-PNG@users.noreply.github.com>
2025-11-30 16:36:52 -0500

    re-did and finished (should be) both company and user DAOs, each with ample documentation for usage. Also made a UserRecord and CompanyRecord for the loadData() methods in user and company DAOs.

--- src/main/java/entity/UserFactory.java
+++ src/main/java/entity/UserFactory.java
@@ -1,7 +1,10 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class UserFactory {
-    public User createEntity(String name, String password, String location, String email, String phone) {
-        return new User(name, password, location, email, phone);
+    public User createEntity(String name, String username, String password, String location,
+                             String email, String phone, ArrayList<Entity> resumes) {
+        return new User(name, username, password, location, email, phone, resumes);
     }
 }
