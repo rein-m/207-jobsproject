0e0ef8dee95cd73f06d47f5b7da25d00b3b05ffe
Rein McKelway <rein.mckelway@mail.utoronto.ca>
2025-11-30 22:48:48 -0500

    stanley is beautiful

--- src/main/java/interface_adapter/loggedin/LoggedinViewModel.java
+++ src/main/java/interface_adapter/loggedin/LoggedinViewModel.java
@@ -3,7 +3,7 @@ package interface_adapter.loggedin;
 import interface_adapter.ViewModel;
 import interface_adapter.login.LoginState;
 
-public class LoggedinViewModel extends ViewModel<LoginState> {
+public class LoggedinViewModel extends ViewModel<LoggedinState> {
     public LoggedinViewModel() {
         super("log in");
         setState(new LoggedinState());
