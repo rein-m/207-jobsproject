f3f3dd376d97253c8a95ed47091ccf74fd6424df
Gabriel Andrus <72159679+GabrielAndrus@users.noreply.github.com>
2025-11-30 21:15:45 -0500

    changed resumes to array of strings
    changed resumes to array of strings

--- src/main/java/entity/UserFactory.java
+++ src/main/java/entity/UserFactory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UserFactory {
     public User createEntity(String name, String username, String password, String location,
-                             String email, String phone, ArrayList<Entity> resumes) {
+                             String email, String phone, ArrayList<String> resumes) {
         return new User(name, username, password, location, email, phone, resumes);
     }
 }
